a3fa1a19d4b433906911c0e38ed83508ce75b86c
leehyeonho <dlgusgh0812@gmail.com>
2022-10-27 16:24:31 +0900

    FIX calendar

--- src/calendar.js
+++ src/calendar.js
@@ -2,19 +2,23 @@ const calendarContainer = document.querySelector(".datetime-extend__calendar__ca
 
 function getCalendar(year, month) {
     const calendarDate = new Date(year, month, 1);
-    console.log(calendarDate);
+    console.log(`calendarDate: ${calendarDate}`);
     const calendarYear = calendarDate.getFullYear();
     const calendarMonth = calendarDate.getMonth()+1;
     const calendarStartDayOfTheWeek = calendarDate.getDay();
-    console.log(calendarStartDayOfTheWeek);
+    console.log(`calendarStartDayOfTheWeek: ${calendarStartDayOfTheWeek}`);
+
+    const calendarLastDate = new Date(year, month+1, 0);
+    const calendarLastDay = calendarLastDate.getDate();
+    console.log(`calendarLastDay: ${calendarLastDay}`);
     
-    const monthLastDate = new Date(year, month-1, 0);
-    const calendarLastDay = monthLastDate.getDate();
+    const monthLastDate = new Date(year, month, 0);
+    console.log(`monthLastDate: ${monthLastDate}`);
 
     const calendarWeekCount = Math.ceil((calendarStartDayOfTheWeek + calendarLastDay) / 7);
     // calendarContainer.querySelector(".blank").remove();
     // calendarContainer.querySelector(".day").remove();
-    for (let i = calendarStartDayOfTheWeek; i >= 0; i--) {
+    for (let i = calendarStartDayOfTheWeek; i > 0; i--) {
         const tag = document.createElement("li")
         tag.classList.add("calendar-item","blank");
         tag.innerText = calendarLastDay-i;
